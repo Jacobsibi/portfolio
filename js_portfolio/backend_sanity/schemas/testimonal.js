2dee50f07faabae65be76cfe9f0bc2c2238c0398
Jacob <jacobssibi@gmail.com>
2022-08-08 11:56:32 +1200

    Initial commit

---
+++ js_portfolio/backend_sanity/schemas/testimonal.js
@@ -0,0 +1,33 @@
+// Entire schema is one object
+export default {
+    //properties
+    name: 'testimonals',
+    title: 'Testimonals',
+    type: 'document',
+    fields: [   //array of fields
+        {   //object
+            name: 'name', //name of person leaving testimonial
+            title: 'Name',
+            type: 'string'
+        },
+        {   
+            name: 'company', 
+            title: 'Company',
+            type: 'string'
+        },
+        {   
+            name: 'imageurl', 
+            title: 'ImgURL',
+            type: 'image',
+            //must use options with image
+            options: { //object
+                hotspot: true, //user can use crop when uploading image
+            }
+        },
+        {   
+            name: 'feedback', 
+            title: 'Feedback',
+            type: 'string'
+        },
+    ],
+}
