74d9c7bc9325d4fe842774746e68d7a2221e0c17
Jacob <jacobssibi@gmail.com>
2022-08-08 17:33:48 +1200

    re-commit

---
+++ frontend_react/src/container/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './About.scss';
+
+const About = () => {
+    return (
+        <div>
+           
+        </div>
+    )
+    imp
+}
+
+export default About
---
+++ frontend_react/src/index.js
@@ -0,0 +1,10 @@
+    import React from 'react';
+    import ReactDOM from 'react-dom'; //help connect with HTML file
+
+    import App from './App';
+    import './index.css';
+
+    //connect App to index.html div with id of root
+    ReactDOM.render(<App />, document.getElementById('root'));
+
+
