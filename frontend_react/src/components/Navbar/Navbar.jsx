38dff6a687048be3ed130525076d44dd549546fa
Jacob <jacobssibi@gmail.com>
2022-08-10 13:29:36 +1200

    navbar styling commit

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             <HiX onClick={() => setToggle(false)} />
             <ul>
               {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                <li key={item}>
+                <li key={item}> 
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
                   </a>
