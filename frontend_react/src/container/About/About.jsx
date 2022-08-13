092c027384c18c275bcc2dc99e5d60bb06492143
Jacob <jacobssibi@gmail.com>
2022-08-13 20:43:52 +1200

    re-commit

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -6,11 +6,24 @@ import './About.scss';
 const About = () => {
     //return react fragment
     return (
-        <div>
-           About
-        </div>
-    )
-    
-}
+        <>
+        <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default About
