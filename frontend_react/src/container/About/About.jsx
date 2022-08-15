2de7ff7ff6a27fb06ac9f0334cf5b6d57d7a77bc
Jacob <jacobssibi@gmail.com>
2022-08-15 16:04:18 +1200

    re-commit
    about styling commit

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -14,11 +14,6 @@ const abouts = [
 
 ];
 
-
-
-
-
-
 const About = () => {
     //return react fragment
     return (
@@ -38,7 +33,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <img src={(about.imgUrl)} alt={about.title} />
             {/* In-line styling  */}
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
